aa15a0f58950ec32dedbb74c9ffb73675b6eadd2
Elbikam <d.elbikam@gmail.com>
2025-01-31 11:09:00 +0100

    Design project using Cursor Editor

--- Nina_Bazar/settings.py
+++ Nina_Bazar/settings.py
@@ -89,13 +89,23 @@ WSGI_APPLICATION = "Nina_Bazar.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "MYDB",
+        "USER": "belbikam",
+        "PASSWORD": "Bob076824202",  # optional if using pgpass or service file
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
